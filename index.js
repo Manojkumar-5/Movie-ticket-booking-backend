33493a5575f9d49bfea1bd0df5bc26b3c6dfbf46
Manojkumar-5 <66167395+Manojkumar-5@users.noreply.github.com>
2021-01-12 23:17:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose
 const app = express();
 
 // set our port to either a predetermined port number if you have set it up, or 3001
-const API_PORT = process.env.API_PORT || 3000;
+const API_PORT = process.env.PORT || 3000;
 // now we should configure the API to use bodyParser and look for JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,5 +26,10 @@ app.use(cors());
 
 // Use our router configuration when we call /api
 app.use(router);
+
+ app.get('/',function(req,res)
+      {
+        res.send('welcome to ticket booking application       1.User Signup \n      2.User Login \n     3.Ticket Dashboard \n     4.Ticket Status \n      5.Booking Details \n       6.Admin ');
+      });
 
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
